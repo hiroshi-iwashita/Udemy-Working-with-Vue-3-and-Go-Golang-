948e7ecdabb9f0dac2c1bae3c65f89733f378c4e
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-08-09 13:46:32 +0900

    create components related to form

--- vue-app/src/components/Body.vue
+++ vue-app/src/components/Body.vue
@@ -2,7 +2,11 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <img alt="Book and glasses" src="./../assets/book-and-glasses.png" class="img-fluid mt-5">
+                <img
+                    alt="Book and glasses"
+                    src="./../assets/book-and-glasses.png"
+                    class="img-fluid mt-5"
+                >
             </div>
         </div>
     </div>
---
+++ vue-app/src/components/forms/SelectInput.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="mb-3">
+        <label
+            :for="name"
+            class="form-label"
+        >
+            {{label}}
+        </label>
+        <select
+            :id="name"
+            class="form-select"
+            :name="name"
+            :required="required"
+        >
+            <option
+                v-for="option in items"
+                :key="option.value"
+                :value="option.value"
+            >
+                {{option.text}}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SelectInput',
+    props: [
+        "items",
+        "name",
+        "required",
+        "label"
+    ],
+}
+</script>
