f5b22ad83730fdab6b4cd6b1d25e05c6f8671cc2
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-08-09 09:38:27 +0900

    commit

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,10 +1,23 @@
 <template>
-  <h1>My application</h1>
+  <div>
+    <Header />
+    <Body />
+    <Footer />
+  </div>
 </template>
 
 <script>
-export default {
+import Header from "./components/Header.vue"
+import Body from "./components/Body.vue"
+import Footer from "./components/Footer.vue"
 
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Body,
+    Footer,
+  }
 }
 </script>
 
