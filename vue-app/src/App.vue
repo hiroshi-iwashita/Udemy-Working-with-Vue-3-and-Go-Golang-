56e05963602ee817e0bc8a7fb5b401df640acf01
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-08-11 08:39:23 +0900

    import store

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -11,12 +11,18 @@
 <script>
 import Header from "@/components/Header.vue"
 import Footer from "@/components/Footer.vue"
+import { store } from '@/components/store.js'
 
 export default {
   name: 'App',
   components: {
     Header,
     Footer,
+  },
+  data() {
+    return {
+      store
+    }
   }
 }
 </script>
