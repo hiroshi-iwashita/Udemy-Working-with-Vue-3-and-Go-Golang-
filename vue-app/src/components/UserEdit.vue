780a527df905ee91b06e2cb4639c01e18488b55f
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-08-11 18:50:11 +0900

    add buttons

--- vue-app/src/components/UserEdit.vue
+++ vue-app/src/components/UserEdit.vue
@@ -59,6 +59,36 @@
                         name="password">
                     </text-input>
                     <hr>
+
+                    <div class="float-start">
+                        <input
+                            type="submit"
+                            class="btn btn-primary me-2"
+                            value="Save"
+                        >
+                        <router-link
+                            to="/admin/users"
+                            class="btn btn-outline-secondary"                           
+                        >
+                            Cancel
+                        </router-link>
+                    </div>
+
+                    <div class="float-end">
+                        <a
+                            v-if="
+                                (this.$route.params.userId > 0)
+                                && (parseInt(String(this.$route.params.userId), 10) !== store.user.id)
+                            "
+                            class="btn btn-danger"
+                            href="javascript:void(0);"
+                            @click="confirmDelete(this.user.id)"
+                        >
+                            Delete
+                        </a>
+                    </div>
+                    <div class="clearfix"></div>
+
                 </form-tag>
             </div>
         </div>
@@ -69,7 +99,8 @@
 import Security from '@/components/security.js'
 import FormTag from './forms/FormTag.vue'
 import TextInput from './forms/TextInput.vue'
-// import notie from 'notie'
+import notie from 'notie'
+import { store } from '@/components/store.js'
 
 export default {
     beforeMount() {
@@ -88,7 +119,8 @@ export default {
                 last_name: "",
                 email: "",
                 password: "",
-            }
+            },
+            store,
         }
     },
     components: {
@@ -97,6 +129,37 @@ export default {
     },
     methods: {
         submitHandler() {
+            const payload = {
+                id: parseInt(String(this.$route.params.userId), 10),
+                first_name: this.user.first_name,
+                last_name: this.user.last_name,
+                email: this.user.email,
+                password: this.user.password,
+            }
+
+            fetch(`${process.env.VUE_APP_API_URL}/admin/users/save`, Security.requestOptions(payload))
+            .then((response) => response.json())
+            .then((response) => {
+                if (response.error) {
+                    notie.alert({
+                        type: 'error',
+                        text: response.message,
+                    })
+                } else {
+                    notie.alert({
+                        type: 'success',
+                        text: 'Changes saved!',
+                    })
+                }
+            })
+            .catch((error) => {
+                 notie.alert({
+                    type: 'error',
+                    text: error,
+                })
+            })
+        },
+        confirmDelete() {
 
         }
     }
