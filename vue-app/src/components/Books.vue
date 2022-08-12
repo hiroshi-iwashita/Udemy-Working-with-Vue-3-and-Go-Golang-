9d0b78a030eeda6d325be2c5c212b5efe82872d3
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-08-12 11:58:38 +0900

    add style for transition group

--- vue-app/src/components/Books.vue
+++ vue-app/src/components/Books.vue
@@ -57,7 +57,11 @@
 
                 <div> <!-- display books -->
                     <div class="card-group">
-                        <div class="p-3 d-flex flex-wrap">
+                        <transition-group
+                            class="p-3 d-flex flex-wrap"
+                            tag="div"
+                            appear name="books"
+                        >
                             <div
                                 v-for="b in this.books"
                                 :key="b.id"
@@ -97,9 +101,8 @@
                                     </small>
                                     </div>
                                 </div>
-
                             </div>
-                        </div>
+                        </transition-group>
                     </div>
                 </div> <!-- display books -->
 
@@ -168,7 +171,25 @@ export default {
     background: lightgray;
 }
 
+
 .book-author, .book-genre {
     font-size: 0.8em;
+}
+
+/* transition styles */
+.books-move {
+    transition: all 500ms ease-in-out 50ms;
+}
+
+.books-enter-active {
+    transition: all 500ms ease-in-out;
+}
+
+.books-leave-active {
+    transition: all 500ms ease-in;
+}
+
+.books-enter, .books-leave-to {
+    opacity: 0;
 }
 </style>
